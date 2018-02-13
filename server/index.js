22b3eb276521230b4aa764735707340706bf10c7
Sara Smith <sarasmith@Saras-MacBook-Air.local>
2018-02-13 14:56:50 -0600

    1-12, need to debug

---
+++ server/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+require ('dotenv').config();
+
+const checkForSession = require('./middlewares/checkForSession');
+
+const swag_controller = require('./controllers/swag_controller');
+const auth_controllers = require('./controllers/auth_controllers');
+const cart_controller = require('./controllers/cart_controller');
+const search_controller = require('./controllers/search_controller');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+}));
+app.use(checkForSession);
+app.use(express.static(`${__dirname}/build`));
+
+app.get('/api/swag', swag_controller.read);
+
+app.post('/api/login', auth_controllers.login);
+app.post('/api/register', auth_controllers.register);
+app.post('/api/signout', auth_controllers.signout);
+app.get('/api/user', auth_controllers.getUser);
+
+app.post('/api/cart', cart_controller.add);
+app.post('/api/cart/checkout', cart_controller.checkout);
+app.delete('/api/cart', cart_controller.delete);
+
+app.get('/api/search', search_controller.search);
+
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {console.log(`Server listening on port ${port}.`); }); 
